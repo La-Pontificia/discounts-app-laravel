33e74c9eb537fbdef5f41ac56a4648ccdf577c5c
Daustinn <daustinn.music@gmail.com>
2024-12-05 11:36:18 -0500

    Refactor routes to add authentication middleware and controllers for dashboard, users, and clients

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Middleware\AuthMiddleware;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::post('login', [LoginController::class, 'login']);
+Route::middleware([AuthMiddleware::class])->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::post('/users/{id}', [UserController::class, 'update']);
+    Route::post('/users/{id}/toggle-status', [UserController::class, 'toggleStatus']);
+
+    Route::get('/clients', [ClientController::class, 'index']);
 });
