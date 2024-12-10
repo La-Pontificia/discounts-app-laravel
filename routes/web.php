9abffb797a3804ab31329ced301c785d4ad2cbca
Daustinn <daustinn.music@gmail.com>
2024-12-10 10:42:29 -0500

    Refactor routes to include DiscountController for managing discounts

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DiscountController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Middleware\AuthMiddleware;
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 Route::post('login', [LoginController::class, 'login']);
 Route::middleware([AuthMiddleware::class])->group(function () {
-    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/dashboard', [DashboardController::class, 'index']);
 
     Route::get('/users', [UserController::class, 'index']);
@@ -21,8 +22,15 @@ Route::middleware([AuthMiddleware::class])->group(function () {
     Route::post('/users/{id}/toggle-status', [UserController::class, 'toggleStatus']);
 
     Route::get('/clients', [ClientController::class, 'index']);
+    Route::get('/clients/one', [ClientController::class, 'one']);
     Route::post('/clients', [ClientController::class, 'store']);
     Route::post('/clients/import', [ClientController::class, 'import']);
     Route::post('/clients/{id}', [ClientController::class, 'update']);
     Route::post('/clients/{id}/toggle-status', [ClientController::class, 'toggleStatus']);
+
+
+    Route::get('/discounts', [DiscountController::class, 'index']);
+    Route::post('/discounts', [DiscountController::class, 'store']);
+    Route::post('/discounts/{id}', [DiscountController::class, 'update']);
+    Route::post('/discounts/{id}/delete', [DiscountController::class, 'destroy']);
 });
